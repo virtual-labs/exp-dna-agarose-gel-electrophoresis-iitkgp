aca1cc055d1913962c45c027485f7f732f4ecae8
prakriti5dhang <prakritidhang@gmail.com>
2023-09-04 17:55:04 +0530

    agarosegel

--- experiment/simulation/js/main.js
+++ experiment/simulation/js/main.js
@@ -1,44 +1,63 @@
-//Your JavaScript goes in here
+
+/*  
+Lab name: Experimental Biochemistry
+Exp name: DNA analysis by agarose gel electrophoresis
+File name: main.js
+Developer: Prakriti Dhang
+*/
+
+function start() {
+  document.getElementById("gelprep").disabled = false;
+  alert("A desktop or laptop is the preferred choice for an enhanced viewing experience.")
+  document.getElementById("start").disabled = true;
+}
+
+
+
 var image;
 var image1;
 function moveImage() {
-    // Calculate the new position (adjust these values as needed)
-    var topimg = 111 ; 
-    var lefting = 98 ; 
-image = document.getElementById('agel');
+  // Calculate the new position (adjust these values as needed)
+  var topimg = 223;
+  var lefting = 14;
+  image = document.getElementById('agel');
   image.style.top = topimg + '%';
-  image.style.left = lefting + '%';  //`translate(${top}%, ${left}%)`;
-  image.style.width= 65 + '%';
-  
-  if((top == 95) && (left = 11)){
+  image.style.left = lefting + '%';  
+  //image1.style.display = "block";
+  //image.style.transform=`translate(${topimg}%, ${lefting}%)`;
+  /* if((top == 95) && (left = 11)){
     image1.style.display="block";
-    image.style.display="none";
-  }
-  
+    
+  } */
+
 }
 
 function allowDrop(event) {
-    event.preventDefault();
-  }
-  
-  function drag(event) {
-    event.dataTransfer.setData("text", event.target.id);
-  }
-  
-  function drop(event) {
-    event.preventDefault();
-    var data = event.dataTransfer.getData("text");
-    var image = document.getElementById(data);
-    event.target.appendChild(image);
-  }
-
-  function moveImage1() {
-    // Calculate the new position (adjust these values as needed)
-    var top = 200 ; 
-    var left = 310 ; 
- image1   = document.getElementById('agel1');
-  image1.style.transform = `translate(${top}%, ${left}%)`;
-  //image1.style.width= 69 + '%';
-  image.style.display="none";
-  
+  event.preventDefault();
 }
+
+function drag(event) {
+  event.dataTransfer.setData("text", event.target.id);
+}
+
+function drop(event) {
+  event.preventDefault();
+  var data = event.dataTransfer.getData("text");
+  var image = document.getElementById(data);
+  event.target.appendChild(image);
+}
+
+function moveImage1() {
+  // Calculate the new position (adjust these values as needed)
+  var topimg1 = 365;
+  var leftimg = 18;
+  image1 = document.getElementById('agel1');
+  image1.style.top = topimg1 + '%';
+  image1.style.left = leftimg + '%';
+  //image1.style.transform = `translate(${top}%, ${left}%)`;
+  //image1.style.width= 69 + '%';
+  image.style.display = "none";
+
+}
+
+
